edf9cbde1434e52ad0926e3fbcaea388234bfc6f
Andre S. Hansen <andre.ok@online.no>
2011-09-22 14:59:21 +0200

    Pre League Start site.
    Signed-off-by: Andre S. Hansen <andre.ok@online.no>

---
+++ data.js
@@ -0,0 +1,16 @@
+var matches =[/* Player A | A sets | B sets | Player B | Date played*/
+             ];
+
+var players = [
+               ["Andriod",0,0,0,0,0,0,0,0],
+               ["Nelich",0,0,0,0,0,0,0,0],
+               ["Selmer",0,0,0,0,0,0,0,0],
+               ["TheForgottenOne",0,0,0,0,0,0,0,0],
+               ["Steinar",0,0,0,0,0,0,0,0],
+               ["Jostein",0,0,0,0,0,0,0,0],
+               ["Krakels",0,0,0,0,0,0,0,0],
+               ["Dagz",0,0,0,0,0,0,0,0],
+               ["Sorebo",0,0,0,0,0,0,0,0],
+               ["Christian",0,0,0,0,0,0,0,0],
+               ["Emil",0,0,0,0,0,0,0,0]
+               ];
