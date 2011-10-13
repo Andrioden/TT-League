c183a19e33a43ebdb22a5a3b17f9ecd0e70abf77
Andre S. Hansen <andre.ok@online.no>
2011-10-13 16:22:32 +0200

    Round 2
    Signed-off-by: Andre S. Hansen <andre.ok@online.no>

--- data.js
+++ data.js
@@ -2,7 +2,16 @@ var matches =[/* Player A | A sets | B sets | Player B | Date played*/
               ['Selmer', 2, 1, 'Emil', new Date(2011,9,6)],
               ['Sorebo', 2, 0, 'Nelich', new Date(2011,9,6)],
               ['Jostein', 1, 2, 'Steinar', new Date(2011,9,6)],
-              ['Andriod', 1, 2, 'Christian', new Date(2011,9,6)]
+              ['Andriod', 1, 2, 'Christian', new Date(2011,9,6)],
+              
+              ['Jostein', 0, 2, 'Christian', new Date(2011,9,13)],
+              ['Steinar', 1, 2, 'Sorebo', new Date(2011,9,13)],
+              ['Andriod', 2, 1, 'Nelich', new Date(2011,9,13)],
+              ['Emil', 2, 0, 'Håkon', new Date(2011,9,13)],
+              ['Steinar', 0, 2, 'Sorebo', new Date(2011,9,13)],
+              ['Jostein', 0, 2, 'Nelich', new Date(2011,9,13)],
+              ['Håkon', 0, 2, 'Christian', new Date(2011,9,13)],
+              ['Emil', 1, 2, 'Andriod', new Date(2011,9,13)],
               ];
 
 var players = [
@@ -17,5 +26,6 @@ var players = [
                ["Sorebo",0,0,0,0,0,0,0,0],
                ["Christian",0,0,0,0,0,0,0,0],
                ["Emil",0,0,0,0,0,0,0,0],
-               ["Mikael",0,0,0,0,0,0,0,0]
+               ["Mikael",0,0,0,0,0,0,0,0],
+               ["Håkon",0,0,0,0,0,0,0,0]
                ];
